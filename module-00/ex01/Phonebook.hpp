b0c64e00abaaa3788a291f210b53b4444b684585
f0lio <36214338+f0lio@users.noreply.github.com>
2021-11-22 00:43:41 +0100

    init project; working on module-00

---
+++ module-00/ex01/Phonebook.hpp
@@ -0,0 +1,23 @@
+#ifndef _PHONEBOOK_
+# define _PHONEBOOK_
+
+class Phonebook
+{
+private:
+    /* data */
+public:
+    Phonebook(/* args */    );
+    ~Phonebook();
+};
+
+Phonebook::Phonebook(/* args */)
+{
+}
+
+Phonebook::~Phonebook()
+{
+}
+
+
+
+#endif
---
+++ module-00/ex01/phonebook.cpp
@@ -0,0 +1,15 @@
+
+#include "Phonebook.hpp"
+
+int main(void)
+{
+    
+    return 0;
+}
+
+
+
+char uppercase(unsigned int i, char c)
+{
+    return c
+}
