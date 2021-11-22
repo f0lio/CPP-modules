f34e0226030a8e25040c65cb1619243909de7dc5
f0lio <36214338+f0lio@users.noreply.github.com>
2021-11-22 02:54:50 +0100

    add ex00, and subject of module-00

---
+++ module-00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		std::string str(argv[i]);
+		for (size_t j = 0; j < str.length(); j++)
+			str[j] = (char)std::toupper(str[j]);
+		std::cout << str;
+	}
+	std::cout << std::endl;
+	return 0;
+}
